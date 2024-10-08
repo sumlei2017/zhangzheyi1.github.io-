6e4826f231742651cb479232f51986a520e03806
sumlei2017 <sumlei@outlook.com>
2024-10-09 05:17:49 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+{
+            "id": 1070,
+            "time": "2024-05-13 14:55:21",
+            "channel_name": "sutui",
+            "status": 1,
+            "jpath_type": 1,
+            "sort": 1001,
+            "index_image_url": "http://we-video.oss-cn-shanghai.aliyuncs.com/1724147008886WechatIMG1760.jpg",
+            "video_url": "",
+            "comment": "",
+            "jpath": "https://snvazev2ds.feishu.cn/wiki/Ao9QwhGU4iTPnhkg3QpcM6rNnQd?from=from_copylink"
+        }
